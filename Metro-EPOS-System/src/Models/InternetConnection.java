3c6514d9f42a5fd696ba1363adf18400c05739be
Faizan Saleh <116947935+FaizanMirza123@users.noreply.github.com>
2024-11-11 22:28:37 +0500

    InternetConnection for Sql
    IP and port set in a class Named Internet Connection and also depdendency set in pom.xml (Ignore if shows error, some IDE issue)

---
+++ Metro-EPOS-System/src/Models/InternetConnection.java
@@ -0,0 +1,26 @@
+package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class InternetConnection {
+
+    private static final String JDBC_URL = "jdbc:sqlserver://103.31.104.114:1433;databaseName=SCD_DB;user=SCD_User;password=12345678;encrypt=true;trustServerCertificate=true;";
+
+    public static Connection getConnection() throws SQLException {
+
+        Connection con = DriverManager.getConnection(JDBC_URL);
+        System.out.println("Connection established successfully!");
+        return con;
+    }
+    /* For Debugging Purpose ONLY
+    public static void main(String args[]) {
+        try {
+            Connection con = getConnection();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }*/
+}
