6d820c964a5264e8bc9e1c22ef9b21e61d1ce70f
Asfandyar001 <l227945@lhr.nu.edu.pk>
2024-11-15 22:17:11 +0500

    Made private variables and Planned methods to implement

--- Metro-EPOS-System/src/Views/Cashier/SalesData.java
+++ Metro-EPOS-System/src/Views/Cashier/SalesData.java
@@ -1,9 +1,47 @@
 package Views.Cashier;
 import Views.Decorate.Theme;
 
-public class SalesData extends Theme {
+import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
-    public SalesData(){
-        
+public class SalesData extends Theme {
+    private BufferedImage image;
+    private JLabel user;
+    private JLabel branchID;
+    private JLabel total;
+    private JScrollPane scroll;
+    private JTextField discountBox;
+    private JTextField productID;
+    private JTextField quantity;
+
+    private JButton logoutButton;
+    private JButton enterButton;
+    private JButton resetButton;
+
+    public SalesData(String name, String branchID)
+    {
+        super.setLineSize5(313,120);
+        super.setLineSize5(313,138);
+        super.setLineSizeCustom(313,306,3);
+        super.setLineSizeCustom(313,351,3);
+        super.setLineSizeCustom(313,399,2);
+        super.setText("Cashier");
+        super.setLogoutLogo();
+        super.setProfileLogo("Images/CashierLogo.png");
+        super.setRectangle(48,272);
+
+        setNames(name,branchID);
+        setHeading();
+        setFields();
+        setBottons();
+
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                requestFocusInWindow();
+            }
+        });
     }
 }
