ff082b22d28e3dec21b23ca70bcdb6cbf82d2a03
Asfandyar001 <l227945@lhr.nu.edu.pk>
2024-11-16 01:48:46 +0500

    Added Functionalities of enter and fix button v2

--- Metro-EPOS-System/src/Views/UIHandler.java
+++ Metro-EPOS-System/src/Views/UIHandler.java
@@ -1,4 +1,24 @@
 package Views;
 
-public class UIHandler {
+public class UIHandler
+{
+    public static boolean isProductExist(int pID)
+    {
+        // Against pID I should get boolean value either the pID exist or not in tables
+        return true;
+    }
+
+    public static String getProductPrice(int pID, int qty)
+    {
+        // I will provide qty -> quantity and pID -> Product ID and I should get the price of product
+        // Here I assume 100 is the price I get for one product pID (later converted to string after multiply)
+        return String.valueOf(qty * 100);
+    }
+
+    public static String getProductName(int pID)
+    {
+        // I provide the pID and I should get the name of the requested Product
+        // Here I assume I get Paint as product name
+        return "Paint";
+    }
 }
